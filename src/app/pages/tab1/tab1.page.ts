161ec43bad6aa2b7dc7b467f66c1a6bd4c1310e6
Humberto Arriaga <arriagar@gmail.com>
2020-06-25 00:46:01 -0500

    base app modificada con 2 tabs

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { DeseosService } from 'src/app/services/deseos.service';
+import { Lista } from 'src/app/models/lista.model';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  listas: Lista[];
+  constructor(private deseos: DeseosService) {
+    this.listas = deseos.listas;
+  }
+
+}
